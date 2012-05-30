723bdca088d3c1f2dea1f60a23e610ee99c4853c
Jose <jose@jose-notebook.(none)>
2012-05-30 12:06:32 +0200

    adding pilotos

--- proyecto/MotoGP/MotoGP/items.py
+++ proyecto/MotoGP/MotoGP/items.py
@@ -22,4 +22,22 @@ class Circuito(Item):
     
     records = Field()
     
+
+class Piloto(Item):
+
+    nombre = Field()
+    lugar_nacimiento = Field()
+    fecha_nacimiento = Field()
+    peso = Field()
+    altura = Field()
+    equipo = Field()
+    moto = Field()
+    dorsal = Field()
+    pais = Field()
+
+    sumario = Field()
+    estadisticas = Field()
+    perfil = Field()
+    
+    
     
